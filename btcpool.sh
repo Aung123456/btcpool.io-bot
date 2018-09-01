afa54dea999ae06ea4905f6eae4c9ccf05ced73d
Evelyn Carroll <30286841+agusgans@users.noreply.github.com>
2018-09-01 23:54:06 +0800

    Update btcpool.sh

--- btcpool.sh
+++ btcpool.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 clear
+printf "Insert Reff ID: "; read Reff
 reff(){
 pin=$(shuf -i 1000-9999 -n 1)
 address=$(curl "https://www.blockchain.com/btc/unconfirmed-transactions" -s | grep -Po '(?<=<a href="/btc/address/)[^"]*' | sed -n 1p)
-signup=$(curl -s -X POST -d "wallet_address=$address&pin=$pin&action=post&refid=230483" --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:53.0) Gecko/20100101 Firefox/53.0" "https://btcpool.io/ajax/sign" -e "https://web.facebook.com/" -x "$proxys" -m 10 2> /dev/null)
+signup=$(curl -s -X POST -d "wallet_address=$address&pin=$pin&action=post&refid=$Reff" --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:53.0) Gecko/20100101 Firefox/53.0" "https://btcpool.io/ajax/sign" -e "https://web.facebook.com/" -x "$proxys" -m 10 2> /dev/null)
     if [[ $signup == "" ]]
         then
    printf "[Address: $address] [Proxy: $proxys]"
